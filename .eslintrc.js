e4271e58a29119d218c44138bbc27cc7c97498b5
Jimmy <jimmy@Jimmys-Mac-mini.homenet.telecomitalia.it>
2021-07-31 12:56:31 +0200

    asdas

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,6 @@ module.exports = {
 		jest: true,
 	},
 	rules: {
-        'no-console': 'off',
-        "react/react-in-jsx-scope": "off",
 		'@typescript-eslint/interface-name-prefix': 'off',
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
