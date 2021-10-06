946c02e2569475188f9bc7b5f8c37b1e9066c3db
Jimmy <jimmy@Jimmys-Mac-mini.home>
2021-10-06 10:53:02 +0200

    add missing comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import Todos from "./components/todo/Todos";
+import Todos from './components/todo/Todos';
 
 const App = () => {
-  return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center bg-gray-900 text-white">
-      <h1 className="text-5xl">Happy Hacking!</h1>
-      <Todos />
-    </div>
-  );
+    return (
+        <div className="w-screen h-screen flex flex-col justify-center items-center bg-gray-900 text-white">
+            <h1 className="text-5xl">Happy Hacking!</h1>
+            <Todos />
+        </div>
+    );
 };
 
 export default App;
--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -1,22 +1,22 @@
 type TodoProps = {
-  todo: TodoItem;
+    todo: TodoItem;
 };
 
 export type TodoItem = {
-  done: boolean;
-  id: string;
-  description: string;
+    done: boolean;
+    id: string;
+    description: string;
 };
 
 const Todo = ({ todo }: TodoProps) => {
-  return (
-    <div className="p-4 mb-2 rounded border border-white flex justify-between items-center">
-      <div className="flex-1">
-        <p>{todo.description}</p>
-      </div>
-      <div>{todo.done ? <p>V</p> : <p>X</p>}</div>
-    </div>
-  );
+    return (
+        <div className="p-4 mb-2 rounded border border-white flex justify-between items-center">
+            <div className="flex-1">
+                <p>{todo.description}</p>
+            </div>
+            <div>{todo.done ? <p>V</p> : <p>X</p>}</div>
+        </div>
+    );
 };
 
 export default Todo;
